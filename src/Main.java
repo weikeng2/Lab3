e764103980f915ceaa4eda22e6137040beacec5a
weikeng2 <42951918+weikeng2@users.noreply.github.com>
2018-09-19 18:17:35 -0500

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(WebScraper.urlToString("http://erdani.com/tdpl/hamlet.txt"));
+        System.out.println(WebScraper.countWords("http://erdani.com/tdpl/hamlet.txt"));
+        System.out.println(WebScraper.searchCount("http://erdani.com/tdpl/hamlet.txt", "prince"));
     }
 }
--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,48 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
-    public static void main(String[] unused) {
-        System.out.println("Ran");
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
+    /**
+     * Finds the number of words in a txt file
+     * @param url some url of a text file or something
+     */
+    public static int countWords(final String url) {
+        String file = urlToString(url);
+        String[] words= file.trim().split("\\s");
+        return words.length;
+    }
+
+    /**
+     *
+     */
+    public static int searchCount(final String url, final String compare) {
+        String file = urlToString(url);
+        int count = 0;
+        String[] words = file.split("\\s");
+        for (String word: words) {
+            if (word.replaceAll(",", "").toLowerCase().equals(compare.toLowerCase())) {
+                count++;
+            }
+        }
+        return count;
     }
 }
